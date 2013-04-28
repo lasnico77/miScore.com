274ff135b35002549f5e6ba06e601ee7d59abc5b
JOSE GONZALES <jz.gonzales@gmail.com>
2013-04-28 00:45:56 -0500

    Cambio routing front controller y estilos css.

--- miScore/application/views/welcome_message.php
+++ miScore/application/views/welcome_message.php
@@ -67,18 +67,18 @@
 <body>
 
 <div id="container">
-	<h1>Welcome to CodeIgniter!</h1>
+	<h1>MiScore.com</h1>
 
 	<div id="body">
 		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
+		<div>
+			<a href='<?php echo site_url('examples/customers_management')?>'>Customers</a> |
 
-		<p>If you would like to edit this page you'll find it located at:</p>
-		<code>application/views/welcome_message.php</code>
-
-		<p>The corresponding controller for this page is found at:</p>
-		<code>application/controllers/welcome.php</code>
-
-		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.</p>
+		</div>
+		<div style='height:20px;'></div>  
+	    <div>
+			<?php echo $output; ?>
+	    </div>		
 	</div>
 
 	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds</p>
