adbe53de4b4eaed283a46a35171170b2400925c0
JOSE GONZALES <jz.gonzales@gmail.com>
2013-04-28 13:32:01 -0500

    Se agregan directorios docs/persistencia y se agregan los archivos de modelo ER y los scripts de creacion de la BD con su versionamiento respectivo.

---
+++ miScore/docs/persistencia/ScriptV1.0.sql
@@ -0,0 +1,53 @@
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+
+CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci ;
+USE `mydb` ;
+
+-- -----------------------------------------------------
+-- Table `mydb`.`Equipo`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `mydb`.`Equipo` (
+  `idEquipo` INT NOT NULL AUTO_INCREMENT ,
+  `nombreEquipo` VARCHAR(100) NOT NULL ,
+  PRIMARY KEY (`idEquipo`) )
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`Torneo`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `mydb`.`Torneo` (
+  `idTorneo` INT NOT NULL AUTO_INCREMENT ,
+  `nombreTorneo` VARCHAR(45) NOT NULL ,
+  PRIMARY KEY (`idTorneo`) )
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`Torneo_has_Equipo`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `mydb`.`Torneo_has_Equipo` (
+  `Torneo_idTorneo` INT NOT NULL ,
+  `Equipo_idEquipo` INT NOT NULL ,
+  PRIMARY KEY (`Torneo_idTorneo`, `Equipo_idEquipo`) ,
+  INDEX `fk_Torneo_has_Equipo_Equipo1_idx` (`Equipo_idEquipo` ASC) ,
+  INDEX `fk_Torneo_has_Equipo_Torneo_idx` (`Torneo_idTorneo` ASC) ,
+  CONSTRAINT `fk_Torneo_has_Equipo_Torneo`
+    FOREIGN KEY (`Torneo_idTorneo` )
+    REFERENCES `mydb`.`Torneo` (`idTorneo` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Torneo_has_Equipo_Equipo1`
+    FOREIGN KEY (`Equipo_idEquipo` )
+    REFERENCES `mydb`.`Equipo` (`idEquipo` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
