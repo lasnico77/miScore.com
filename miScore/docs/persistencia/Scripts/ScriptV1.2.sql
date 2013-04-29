582ab385dc11f43640160d866011f1ffe79f6184
Diego <game861128@gmail.com>
2013-04-28 20:22:58 -0500

    se modifico y se realizo un refactor de los create de las tablas

--- miScore/docs/persistencia/Scripts/ScriptV1.2.sql
+++ miScore/docs/persistencia/Scripts/ScriptV1.2.sql
@@ -3,12 +3,12 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
 CREATE SCHEMA IF NOT EXISTS `miScore` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci ;
-USE `mydb` ;
+USE `miScore` ;
 
 -- -----------------------------------------------------
--- Table `mydb`.`Equipo`
+-- Table `miScore`.`Equipo`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `mydb`.`Equipo` (
+CREATE  TABLE IF NOT EXISTS `miScore`.`Equipo` (
   `idEquipo` INT NOT NULL AUTO_INCREMENT ,
   `nombreEquipo` VARCHAR(100) NOT NULL ,
   PRIMARY KEY (`idEquipo`) )
@@ -16,9 +16,9 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`Torneo`
+-- Table `miScore`.`Torneo`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `mydb`.`Torneo` (
+CREATE  TABLE IF NOT EXISTS `miScore`.`Torneo` (
   `idTorneo` INT NOT NULL AUTO_INCREMENT ,
   `nombreTorneo` VARCHAR(45) NOT NULL ,
   `estadoTorneo` TINYINT NOT NULL DEFAULT 1 ,
@@ -27,9 +27,9 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`Torneo_has_Equipo`
+-- Table `miScore`.`Torneo_has_Equipo`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `mydb`.`Torneo_has_Equipo` (
+CREATE  TABLE IF NOT EXISTS `miScore`.`Torneo_has_Equipo` (
   `Torneo_idTorneo` INT NOT NULL ,
   `Equipo_idEquipo` INT NOT NULL ,
   PRIMARY KEY (`Torneo_idTorneo`, `Equipo_idEquipo`) ,
@@ -37,12 +37,12 @@ CREATE  TABLE IF NOT EXISTS `mydb`.`Torneo_has_Equipo` (
   INDEX `fk_Torneo_has_Equipo_Torneo_idx` (`Torneo_idTorneo` ASC) ,
   CONSTRAINT `fk_Torneo_has_Equipo_Torneo`
     FOREIGN KEY (`Torneo_idTorneo` )
-    REFERENCES `mydb`.`Torneo` (`idTorneo` )
+    REFERENCES `miScore`.`Torneo` (`idTorneo` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Torneo_has_Equipo_Equipo1`
     FOREIGN KEY (`Equipo_idEquipo` )
-    REFERENCES `mydb`.`Equipo` (`idEquipo` )
+    REFERENCES `miScore`.`Equipo` (`idEquipo` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
